3db45c0fea373087deca17493429a964e285e7a8
Deilons <apolo4748@gmail.com>
2024-10-03 07:08:15 -0500

    Feat: Order models

---
+++ Models/Order.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace techstore.Models;
+
+[Table("orders")]
+public class Order
+{   
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public DateTime OrderDate { get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int Quantity { get; set; }
+
+    [Required]
+    [Range(0, double.MaxValue)]
+    public decimal TotalAmount { get; set; }
+
+    public int ClientId { get; set; }
+    public Client Client { get; set; }
+
+    public ICollection<Product> Products { get; set; }
+}
+
--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace techstore.Models;
 
-    public class User
+[Table("user")]
+public class User
 {
     [Key]
     public int Id { get; set; }
