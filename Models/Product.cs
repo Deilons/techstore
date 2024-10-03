47510db37d8009771386114afdafa8758e2ed763
Deilons <apolo4748@gmail.com>
2024-10-03 06:51:01 -0500

    Feat: Product model

---
+++ Models/Product.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace techstore.Models;
+
+public class Product
+{   
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(30, ErrorMessage = "Name cannot be longer than 100 characters.")]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(100, ErrorMessage = "Description cannot be longer than 100 characters.")]
+    public string Description { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue)]
+    public decimal Price { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int Quantity { get; set; }
+
+    public string CategoryId{ get; set; }
+    public Category Category { get; set; }
+
+    public ICollection<Order> Orders { get; set; }
+
+
+}
