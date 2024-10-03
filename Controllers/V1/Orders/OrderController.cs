6ce5d15cd8544a8df1aa8476134fd1e2d9e87379
Deilons <apolo4748@gmail.com>
2024-10-03 10:27:06 -0500

    Feat: Controllers template

---
+++ Controllers/V1/Orders/OrderController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace techstore.Controllers.V1.Orders
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        
+    }
+}
